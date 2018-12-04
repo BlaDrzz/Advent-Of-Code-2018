c5052c5bd0db96e766cbe35a740105dde670e39c
pvtHenk <jannesdrijkoningen@gmail.com>
2018-12-04 19:19:45 +0100

    removed my company's copyright notice hehe

--- 2.php
+++ 2.php
@@ -1,44 +1,37 @@
 <?php
-/**
- * Copyright (C) A&C systems nv - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by A&C systems <web.support@ac-systems.com>
- */
-
 $input = file_get_contents('input');
 $arr = explode("\r\n", $input);
 
 
 // PART ONE
-// $charCounts = [
-//     2 => 0,
-//     3 => 0
-// ];
-//
-// $checkTwo = true;
-// $checkThree = true;
-// foreach ($arr as $string) {
-//
-//     foreach (array_unique(str_split($string)) as $char) {
-//         $count = substr_count($string, $char);
-//
-//         if ($count === 2 && $checkTwo) {
-//             $charCounts[2]++;
-//             $checkTwo = false;
-//         } elseif($count === 3 && $checkThree) {
-//             $charCounts[3]++;
-//             $checkThree = false;
-//         }
-//     }
-//     $checkTwo = true;
-//     $checkThree = true;
-// }
-//
-// echo $charCounts[2] * $charCounts[3];
+$charCounts = [
+    2 => 0,
+    3 => 0
+];
+
+$checkTwo = true;
+$checkThree = true;
+foreach ($arr as $string) {
+
+    foreach (array_unique(str_split($string)) as $char) {
+        $count = substr_count($string, $char);
+
+        if ($count === 2 && $checkTwo) {
+            $charCounts[2]++;
+            $checkTwo = false;
+        } elseif ($count === 3 && $checkThree) {
+            $charCounts[3]++;
+            $checkThree = false;
+        }
+    }
+    $checkTwo = true;
+    $checkThree = true;
+}
+
+echo $charCounts[2] * $charCounts[3];
 
 
-// PART TWO
+// PART TWO (UNFINISHED)
 $diffArr = [];
 foreach ($arr as $line) {
     $diff = [];
